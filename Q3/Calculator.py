6820a67ba6fdecfdc13355c72328d67f9e870105
AuspiciousCrane <63011134@kmitl.ac.th>
2022-04-21 17:52:30 +0700

    Implemented Rick Astley

--- Q3/Calculator.py
+++ Q3/Calculator.py
@@ -1,5 +1,6 @@
 class Error(Exception): pass
 class DivideByZeroError(Error): pass
+class NonNumericError(Error): pass
 
 """
 class Calculator:
@@ -7,21 +8,32 @@ class Calculator:
 		self.acc = 0.00
 
 	def set_accumulator(self, a):
+		if not (isinstance(a, int) or isinstance(a, float)):
+			raise NonNumericError("Input is not a number")
 		self.acc = a
 
 	def get_accumulator(self):
 		return self.acc
 
 	def add(self, x):
+		if not (isinstance(x, int) or isinstance(x, float)):
+			raise NonNumericError("Input is not a number")
 		self.acc += x
 
 	def subtract(self, x):
+		if not (isinstance(x, int) or isinstance(x, float)):
+			raise NonNumericError("Input is not a number")
 		self.acc -= x
 
 	def multiply(self, x):
+		if not (isinstance(x, int) or isinstance(x, float)):
+			raise NonNumericError("Input is not a number")
 		self.acc *= x
 
 	def divide(self, x):
+		if not (isinstance(x, int) or isinstance(x, float)):
+			raise NonNumericError("Input is not a number")
+
 		if x == 0:
 			raise DivideByZeroError("You cannot divide a number by zero")
 
